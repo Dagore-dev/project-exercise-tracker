6d9cda6f17db935211eb2fca8e9cd7a510ed0ef0
Dagore <david.gomez.redondo.dev@gmail.com>
2022-08-09 09:26:47 +0200

    routes and controllers defined

---
+++ controllers/usersController.js
@@ -0,0 +1,44 @@
+// eslint-disable-next-line
+const express = require('express')
+/**
+ * You can make a `GET` request to `~/api/users` to get a list of all users. This request response with an array of objects with `username` and `_id` properties.
+ * @param {express.Request} request
+ * @param {express.Response} response
+ */
+function getAllUsers (request, response) {
+  console.error('Not implemented')
+}
+
+/**
+ * You can `POST` to `~/api/users` with form data username to create a new user. The returned response will be an object with `username` and `_id` properties.
+ * @param {express.Request} request
+ * @param {express.Response} response
+ */
+function createNewUser (request, response) {
+  console.error('Not implemented')
+}
+
+/**
+ * You can `POST` to `~/api/users/:_id/exercises` with form data `description`, `duration`, and optionally `date`. If no date is supplied, the current date will be used. The response will be the user object with the exercise fields added.
+ * @param {express.Request} request
+ * @param {express.Response} response
+ */
+function updateUserExercises (request, response) {
+  console.error('Not implemented')
+}
+
+/**
+ * You can make a `GET` request to `~/api/users/:_id/logs` to retrieve a full exercise log of any user. This returns an user object with a `count` property representing the number of exercises that belong to that user and a `log` property which is an array of all exercises added (Each of them consist in an object with the `description`, `duration` and `date` properties).
+ * @param {express.Request} request
+ * @param {express.Response} response
+ */
+function getExercisesFromUser (request, response) {
+  console.error('Not implemented')
+}
+
+module.exports = {
+  getAllUsers,
+  createNewUser,
+  updateUserExercises,
+  getExercisesFromUser
+}
--- index.js
+++ index.js
@@ -1,6 +1,8 @@
+const path = require('path')
 const express = require('express')
 const app = express()
 const cors = require('cors')
+const usersRouter = require('./routes/usersRouter')
 require('dotenv').config()
 
 const PORT = process.env.PORT ?? 3000
@@ -8,12 +10,15 @@ const PORT = process.env.PORT ?? 3000
 app.use(cors())
 app.use(express.static('public'))
 app.get('/', (req, res) => {
-  res.sendFile(__dirname + '/views/index.html')
-});
+  res.sendFile(path.join(__dirname, '/views/index.html'))
+})
 
+// Body parser
+app.use(express.json())
+app.use(express.urlencoded({ extended: true }))
 
-
-
+// Routes
+app.use('/api/users', usersRouter)
 
 const listener = app.listen(PORT, () => {
   console.log('Your app is listening on port ' + listener.address().port)
